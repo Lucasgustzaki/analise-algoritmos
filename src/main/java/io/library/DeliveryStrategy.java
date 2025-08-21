26c936b586714c6eef39de9fa98277db18e2c44c
lemuel-manske <lemuelkauemanskede.liz@philips.com>
2025-08-21 19:23:30 -0300

    remove extra line breaks

--- src/main/java/io/library/DeliveryStrategy.java
+++ src/main/java/io/library/DeliveryStrategy.java
@@ -2,7 +2,5 @@ package io.library;
 
 public sealed interface DeliveryStrategy permits SedexDeliveryStrategy, PACDeliveryStrategy, PickupStrategy {
 
-
-
     Money calculateDelivery(final Order order);
 }

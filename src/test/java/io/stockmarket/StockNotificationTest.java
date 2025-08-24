b2022202693a6238b58794f53c01a95649cb9a82
lemuel-manske <lemuelkauemanskede.liz@philips.com>
2025-08-24 15:10:51 -0300

    rename test class

--- src/test/java/io/stockmarket/StockNotificationTest.java
+++ src/test/java/io/stockmarket/StockNotificationTest.java
@@ -6,7 +6,7 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-class StockTest {
+class StockNotificationTest {
     
     private Stock PETR4;
     private Stock ITUB4;
